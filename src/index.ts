dcfe17e5d263e2e239109bb95a4f8363c8e2cac0
Yuki.Shimoyama <evedon4080@gmail.com>
2021-02-14 20:39:49 +0900

    型エイリアス(type)でオブジェクトの型定義

--- src/index.ts
+++ src/index.ts
@@ -13,18 +13,24 @@
 // primitiveSample()
 
 // 04.関数の型定義
-import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic'
-import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+// import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic'
+// import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
 
-logMessage('Hello TypeScript!')
-logMessage2('Hello TypeScript!')
-logMessage3('Hello TypeScript!')
-logMessage4('Hello TypeScript!')
+// logMessage('Hello TypeScript!')
+// logMessage2('Hello TypeScript!')
+// logMessage3('Hello TypeScript!')
+// logMessage4('Hello TypeScript!')
 
-isUserSignedIn('ABC', 'Torahack')
-isUserSignedIn('DEF')
-isUserSignedIn2('ABC')
-isUserSignedIn2('DEF')
+// isUserSignedIn('ABC', 'Torahack')
+// isUserSignedIn('DEF')
+// isUserSignedIn2('ABC')
+// isUserSignedIn2('DEF')
 
-const sum = sumProductsPrice(100, 200, 300, 400, 500)
-console.log('Function parameters sample 5:', sum)
+// const sum = sumProductsPrice(100, 200, 300, 400, 500)
+// console.log('Function parameters sample 5:', sum)
+
+// 05.オブジェクトの型定義
+import objectSample from './object/object'
+import typeAliasSample from './object/alias'
+objectSample()
+typeAliasSample()
